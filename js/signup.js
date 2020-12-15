1f6af8471f45f701f43da003f19dc63c9d2feddf
Shubham <shubham@itsbhm.group>
2020-12-15 20:58:10 +0530

    async Fixes

--- js/signup.js
+++ js/signup.js
@@ -1,38 +1,41 @@
 $(document).ready(function () {
-    $("#submit").on("click", function (e) {
-        e.preventDefault();
+  $("#signUpFormSubmit").on("click", function (e) {
+    e.preventDefault();
 
-        const uname = $("#username").val();
-        const upass = $("#password").val();
-        const ucpass = $("#cpassword").val();
+    const uname = $("#username").val();
+    const upass = $("#password").val();
+    const ucpass = $("#cpassword").val();
 
-        let arr = { 
-            uname: uname,
-            upass: upass,
-            ucpass: ucpass
-             };
+    let arr = {
+      uname: uname,
+      upass: upass,
+      ucpass: ucpass
+    };
 
-        $.ajax({
-            url: "submit.php",
-            type: "POST",
-            data: JSON.stringify(arr),
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            async: false,
+    $.ajax({
+      url: "submit.php",
+      type: "POST",
+      data: JSON.stringify(arr),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      async: false,
 
-            // success: function (response) {
-            //     const resp = JSON.parse(response);
-            //     if (resp.status == "success") {
-            //       window.location.replace(resp.return_url);
-            //     } else {
-            //       sweetAlert("Dashboard Login", resp.reason, resp.status);
-            //     }
-            //   },
-        
-            //   error: function () {
-            //     sweetAlert("Dashboard Login", "Endpoint Failure", "error");
-            //   }
+      success: function (resp) {
+        // const json_resp = JSON.parse(resp);
 
-        })// Ajax End
-    }) // save-user-data Click End
-}) // Document End
+        if (resp.status == "success") {
+          swal("Signup", resp.msg, resp.status).then(function () {
+            $("#signUpForm").trigger("reset");
+            window.location.replace(resp.return_url);
+          });
+        } else {
+          swal("Signup", resp.msg, resp.status);
+        }
+      },
+
+      error: function () {
+        swal("Signup", "Endpoint Failure", "error");
+      }
+    }); // Ajax End
+  }); // Click Trigger End
+}); // Document End
